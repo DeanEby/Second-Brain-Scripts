761816eace25fe618a4b49347ffd078bd3b2081a
DeanEby <ebyd21@gmail.com>
2024-12-07 00:08:20 -0500

    Implement LDA topic Generation
    Implement LDA gen and remove clutter

--- autolink.py
+++ autolink.py
@@ -1,30 +1,66 @@
 # This python program will allow me to automatically link together notes in my obsidian
 # vault based on how close they are in topic.
-
+from __future__ import print_function
 
 import os
 import re
 import markdown
-import string
 import nltk
+import numpy as np
+import pandas as pd
 nltk.download('stopwords')
-nltk.download('wordnet')  
-nltk.download('omw-1.4')  
+from gensim import corpora, models, similarities 
 from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
 from gensim import corpora
-from gensim.models import LsiModel
-import codecs
-from sklearn import feature_extraction
-import mpld3
 
 
 
 path = "C:/Users/ebyd2/Documents/Obsidian Vault - Copy"
 
-stop = set(stopwords.words('english'))
-exclude = set(string.punctuation)
-lemma = WordNetLemmatizer()
+# taken from http://brandonrose.org/clustering#Latent-Dirichlet-Allocation
+# words that don't impact the topic
+stopwords = stopwords.words('english')
+
+# breaks a word down to its root
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("english")
+
+def tokenize_and_stem(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            # adjusted to remove some of the markdown uglyness that inevitably will be common
+            if not re.search('/', token):
+                if len(token) > 1:
+
+                    filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
+
+def tokenize_only(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            
+            # adjusted to remove some of the markdown uglyness that inevitably will be common
+            if not re.search('/', token):
+                if len(token) > 1:
+
+                    filtered_tokens.append(token)
+    return filtered_tokens
+
+
+
+
+
+
+
 
 #dir_list = os.listdir(path)
 def run_fast_scandir(dir, ext):    # dir: str, ext: list
@@ -48,57 +84,61 @@ def run_fast_scandir(dir, ext):    # dir: str, ext: list
         filepath.extend(fp)
     return subdir, dirpath, files, filepath
 
-# from https://www.datacamp.com/tutorial/what-is-topic-modeling
 
-def clean(doc):
-    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
-    punc_free = "".join(ch for ch in stop_free if ch not in exclude)
-    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
-    return normalized
-
+# These two lists will hold the useful data for our topic modeling
 # I am going to use two related lists instead of a dictionary to make it easier to use the 
 # clean function
-
+# The index of each list will match up.
+# the filepath for each file
 vaultPath = []
+# the contents of each file
 vaultContents = []
+
+# scan the vault
 subdir, dirpath, files, filepath = run_fast_scandir(path, [".md"])
+# loop through all the files in filepath. This allows us to read
+# the text from the files
 for x in filepath:
-    indx = filepath.index(x)
+    #indx = filepath.index(x)
     f = open(x, 'r')
     try:
         htmlmarkdown = markdown.markdown(f.read())
-        #print(htmlmarkdown)
         vaultContents.append(htmlmarkdown)
         vaultPath.append(x)
-        #print(filepath)
-        #print(vaultPath[0])
-        #print(vaultContents[0])
-        #break
     except:
         print("something went wrong")
 
 
+# taken from http://brandonrose.org/clustering#Latent-Dirichlet-Allocation
+tokenized_text = [tokenize_and_stem(text) for text in vaultContents]
+
+texts = [[word for word in text if word not in stopwords] for text in tokenized_text]
+#print(texts)
+
+dictionary = corpora.Dictionary(texts)
+
+#remove extremes
+dictionary.filter_extremes(no_below=1, no_above=0.8)
+
+#convert the dictionary to a bag of words corpus for reference
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+# this could be parallelized
+lda = models.LdaModel(corpus, num_topics=5, 
+                            id2word=dictionary, 
+                            update_every=5, 
+                            chunksize=10000, 
+                            passes=100)
+
+print(lda.show_topics())
 
 
+# # ----- Below currently does not work --------
+# topics_matrix = lda.show_topics(formatted=False, num_words=20)
+# topics_matrix = np.array(topics_matrix)
 
+# topic_words = topics_matrix[:,:,1]
+# for i in topic_words:
+#     print([str(word) for word in i])
+#     print()
 
-# ---------- Replacing --------------
-
-
-clean_corpus = [clean(doc).split() for doc in vaultContents]
-
-#print(clean_corpus)
-dictionary = corpora.Dictionary(clean_corpus)
-doc_term_matrix = [dictionary.doc2bow(doc) for doc in clean_corpus]
-#print(doc_term_matrix)
-
-lsa = LsiModel(doc_term_matrix, num_topics=3, id2word= dictionary)
-
-print(lsa.print_topics(num_topics=3, num_words=3))
-
-from gensim.models import LdaModel
-
-# LDA model
-lda = LdaModel(doc_term_matrix, num_topics=3, id2word = dictionary)
-
-# Results
